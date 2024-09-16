0624ba305c9547e4fd32941fc79c24c66a2c3ac5
monospaced <scott@monospaced.com>
2024-09-16 21:44:43 +0100

    refactor: update to WebAudio API

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-mysteron.js
-========
+# mysteron.js
 
 Web Audio API ‘theremin’, inspired by the early VST freeware Mysteron by FXpansion.
-
-Requires latest Chrome/Safari or iOS 9: [caniuse.com/audio-api](http://caniuse.com/audio-api)
-
-Perpetual work-in-progress and general Web Audio API playground!
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,54 +1,52 @@
 /*global move: true*/
 
 var mysteron = (function () {
-
-  'use strict';
+  "use strict";
 
   var audio,
-      osc,
-      touchpad,
-      nodes = {},
-      ampRate = 0.1,
-      ampDecay = 0.4,
-      pitchRate = 0.2,
-      maxGain = 0.2,
-      minFreq = 16.35, // C0
-      maxFreq = 4186.01, // C8
-      mouseDown = 0,
-      tweaking = false,
-      hasTouch = 'ontouchstart' in window || 'createTouch' in document,
-      eventInit = hasTouch ? 'touchend' : 'DOMContentLoaded',
-      eventStart = hasTouch ? 'touchstart' : 'mousedown',
-      eventMove = hasTouch ? 'touchmove' : 'mousemove',
-      eventEnd = hasTouch ? 'touchend' : 'mouseup',
-      ampTracker,
-      pitchTracker,
-      controls,
-      toggle;
+    osc,
+    touchpad,
+    nodes = {},
+    ampRate = 0.1,
+    ampDecay = 0.4,
+    pitchRate = 0.2,
+    maxGain = 0.2,
+    minFreq = 16.35, // C0
+    maxFreq = 4186.01, // C8
+    mouseDown = 0,
+    tweaking = false,
+    hasTouch = "ontouchstart" in window || "createTouch" in document,
+    eventInit = hasTouch ? "touchend" : "DOMContentLoaded",
+    eventStart = hasTouch ? "touchstart" : "mousedown",
+    eventMove = hasTouch ? "touchmove" : "mousemove",
+    eventEnd = hasTouch ? "touchend" : "mouseup",
+    ampTracker,
+    pitchTracker,
+    controls,
+    toggle;
 
   return {
-
     init: function () {
-
       var doc = document;
 
       try {
-        audio = new window.webkitAudioContext() || window.AudioContext();
+        // Use modern AudioContext
+        audio = new window.AudioContext();
         osc = audio.createOscillator();
       } catch (e) {
-        window.alert('No web audio oscillator support in this browser');
+        window.alert("No web audio oscillator support in this browser");
       }
 
-      touchpad = doc.getElementsByTagName('body')[0];
-      controls = doc.getElementById('controls');
-      toggle = doc.getElementById('toggle');
+      touchpad = doc.getElementsByTagName("body")[0];
+      controls = doc.getElementById("controls");
+      toggle = doc.getElementById("toggle");
 
       nodes.volume = audio.createGain();
       nodes.volume.gain.value = 0;
 
       mysteron.connect();
 
-      toggle.addEventListener('click', mysteron.flip, false);
+      toggle.addEventListener("click", mysteron.flip, false);
 
       mysteron.tweak();
 
@@ -57,168 +55,160 @@ var mysteron = (function () {
       touchpad.addEventListener(eventStart, mysteron.start, false);
 
       if (hasTouch) {
-        doc.body.addEventListener('touchmove', function(e){
-          if (e.target.tagName !== 'INPUT') {
+        doc.body.addEventListener("touchmove", function (e) {
+          if (e.target.tagName !== "INPUT") {
             e.preventDefault();
           }
         });
       }
 
-      controls.addEventListener(eventStart, function(e){
-        e.stopPropagation();
-      }, false);
+      controls.addEventListener(
+        eventStart,
+        function (e) {
+          e.stopPropagation();
+        },
+        false
+      );
 
-      toggle.addEventListener(eventStart, function(e){
-        e.stopPropagation();
-      }, false);
-
+      toggle.addEventListener(
+        eventStart,
+        function (e) {
+          e.stopPropagation();
+        },
+        false
+      );
     },
 
-    connect: function(){
-
+    connect: function () {
       osc.connect(nodes.volume);
       nodes.volume.connect(audio.destination);
-
     },
 
-    flip: function(e){
+    resumeAudioContext: function () {
+      if (audio.state === "suspended") {
+        audio.resume();
+      }
+    },
 
+    flip: function (e) {
       e.preventDefault();
 
       var opacity = tweaking ? 0 : 0.8;
 
       move(controls)
-        .set('visibility', 'visible')
-        .set('opacity', opacity)
-        .duration('0.25s')
-        .end(function(){
+        .set("visibility", "visible")
+        .set("opacity", opacity)
+        .duration("0.25s")
+        .end(function () {
           if (opacity === 0) {
-            controls.setAttribute('style', '');
+            controls.setAttribute("style", "");
           }
           tweaking = !tweaking;
-      });
-
+        });
     },
 
-    tweak: function(){
-
+    tweak: function () {
       var doc = document,
-          ampRateControl = doc.getElementById('ampRate'),
-          ampDecayControl = doc.getElementById('ampDecay'),
-          pitchRateControl = doc.getElementById('pitchRate'),
-          oscControl = doc.getElementById('oscControl');
+        ampRateControl = doc.getElementById("ampRate"),
+        ampDecayControl = doc.getElementById("ampDecay"),
+        pitchRateControl = doc.getElementById("pitchRate"),
+        oscControl = doc.getElementById("oscControl");
 
-      ampRateControl.value = ampRate*100;
-      ampDecayControl.value = ampDecay*100;
-      pitchRateControl.value = pitchRate*100;
+      ampRateControl.value = ampRate * 100;
+      ampDecayControl.value = ampDecay * 100;
+      pitchRateControl.value = pitchRate * 100;
 
-      ampRateControl.addEventListener('change', function(e){
-          ampRate = this.value/100;
+      ampRateControl.addEventListener("change", function (e) {
+        ampRate = this.value / 100;
       });
 
-      ampDecayControl.addEventListener('change', function(e){
-          ampDecay = this.value/100;
+      ampDecayControl.addEventListener("change", function (e) {
+        ampDecay = this.value / 100;
       });
 
-      pitchRateControl.addEventListener('change', function(e){
-          pitchRate = this.value/100;
+      pitchRateControl.addEventListener("change", function (e) {
+        pitchRate = this.value / 100;
       });
 
-      oscControl.addEventListener('change', function(e){
+      oscControl.addEventListener("change", function (e) {
         if (e.target.checked) {
           osc.disconnect();
-          // support both integer and string oscillator types
-
-          // Chrome is no longer throwing an error on integer types
-          // so just using strings now, pending check of iOS
-
-          //try {
-            // iOS still using old integers
-          //  osc.type = e.target.value;
-            // but Chrome will throw an error
-          //} catch(error){
-            // so use new strings from id attribute
-            osc.type = e.target.id;
-          //}
-
+          osc.type = e.target.id;
           mysteron.connect();
         }
       });
-
     },
 
-    track: function(){
-
+    track: function () {
       var e = {},
-          doc = document;
+        doc = document;
 
-      ampTracker = doc.getElementById('amp');
-      pitchTracker = doc.getElementById('pitch');
+      ampTracker = doc.getElementById("amp");
+      pitchTracker = doc.getElementById("pitch");
 
       e.pageY = ampTracker.offsetTop;
       e.pageX = ampTracker.offsetLeft;
 
-      osc.start(0);
+      // The oscillator must be started once
+      osc.start();
 
       window.removeEventListener(eventInit, mysteron.track, false);
 
       mysteron.start(e);
 
-      setTimeout(function(){
+      setTimeout(function () {
         mysteron.stop();
       }, 200);
-
     },
 
-    scale: function(value, oldMin, oldMax, newMin, newMax){
-
-      return (value / ((oldMax - oldMin) / (newMax - newMin))) + newMin;
-
+    scale: function (value, oldMin, oldMax, newMin, newMax) {
+      return value / ((oldMax - oldMin) / (newMax - newMin)) + newMin;
     },
 
-    play: function(e){
+    play: function (e) {
+      mysteron.resumeAudioContext(); // Ensure AudioContext is resumed
 
       var now = audio.currentTime,
-          amp = nodes.volume.gain,
-          pitch = osc.frequency,
-          pageX = e.pageX,
-          pageY = e.pageY,
-          x = mysteron.scale(pageX, 0, window.innerWidth, 0, maxGain),
-          y = mysteron.scale(pageY, 0, window.innerHeight, minFreq, maxFreq);
+        amp = nodes.volume.gain,
+        pitch = osc.frequency,
+        pageX = e.pageX,
+        pageY = e.pageY,
+        x = mysteron.scale(pageX, 0, window.innerWidth, 0, maxGain),
+        y = mysteron.scale(pageY, 0, window.innerHeight, minFreq, maxFreq);
 
       amp.cancelScheduledValues(now);
       amp.setValueAtTime(amp.value, now);
       amp.linearRampToValueAtTime(x, now + ampRate);
 
       move(ampTracker)
-        .set('top', pageY)
+        .set("top", pageY)
         .duration(0)
-        .end(function(){
+        .end(function () {
           move(ampTracker)
-            .set('left', pageX)
+            .set("left", pageX)
             .duration(ampRate * 1000)
             .end();
-      });
+        });
 
       pitch.cancelScheduledValues(now);
       pitch.setValueAtTime(pitch.value, now);
       pitch.linearRampToValueAtTime(y, now + pitchRate);
 
-      pitchTracker.style.display = 'block';
+      pitchTracker.style.display = "block";
 
       move(pitchTracker)
-        .set('left', pageX)
+        .set("left", pageX)
         .duration(0)
-        .end(function(){
+        .end(function () {
           move(pitchTracker)
-            .set('top', pageY)
+            .set("top", pageY)
             .duration(pitchRate * 1000)
             .end();
-      });
-
+        });
     },
 
-    start: function(e){
+    start: function (e) {
+      mysteron.resumeAudioContext(); // Ensure AudioContext is resumed
 
       ++mouseDown;
 
@@ -228,48 +218,41 @@ var mysteron = (function () {
       touchpad.addEventListener(eventEnd, mysteron.stop, false);
 
       if (hasTouch) {
-        touchpad.addEventListener('touchcancel', mysteron.stop, false);
+        touchpad.addEventListener("touchcancel", mysteron.stop, false);
       }
-
     },
 
-    move: function(e){
-
+    move: function (e) {
       if (mouseDown || hasTouch) {
         mysteron.play(e);
       }
-
     },
 
-    stop: function(){
-
+    stop: function () {
       --mouseDown;
 
       var now = audio.currentTime,
-          amp = nodes.volume.gain;
+        amp = nodes.volume.gain;
 
       amp.cancelScheduledValues(now);
       amp.setValueAtTime(amp.value, now);
       amp.linearRampToValueAtTime(0, now + ampDecay);
 
       move(ampTracker)
-        .set('left', 0)
+        .set("left", 0)
         .duration(ampDecay * 1000)
         .end();
 
-      pitchTracker.style.display = 'none';
+      pitchTracker.style.display = "none";
 
       touchpad.removeEventListener(eventMove, mysteron.move, false);
       touchpad.removeEventListener(eventEnd, mysteron.stop, false);
 
       if (hasTouch) {
-        touchpad.removeEventListener('touchcancel', mysteron.stop, false);
+        touchpad.removeEventListener("touchcancel", mysteron.stop, false);
       }
-
-    }
-
+    },
   };
+})();
 
-}());
-
-window.addEventListener('DOMContentLoaded', mysteron.init, true);
+window.addEventListener("DOMContentLoaded", mysteron.init, true);
